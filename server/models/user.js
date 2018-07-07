563cd24a01bbc85eb7a130ee91f5dfecf235c84a
souvikbanerjee86 <souvik.mlindia@gmail.com>
2018-07-07 11:16:42 +0530

    seventh Commit

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
-
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
+const _ = require('lodash');
 
-var User = mongoose.model('User',{
+var UserSchema = new mongoose.Schema({
     email:{
         type:String,
         required:true,
@@ -37,6 +38,44 @@ var User = mongoose.model('User',{
         }
     }]
  });
+
+ UserSchema.methods.toJSON = function(){
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject,['_id','email']);
+ };
+
+ UserSchema.methods.generateAuthToken = function(){
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id:user._id.toHexString(),access},'souvikarpita').toString();
+
+    user.tokens.push({access,token});
+    return user.save().then(()=>{
+        return token;
+    });
+ };
+
+ UserSchema.statics.findByToken = function(token){
+    var User = this;
+    var decoded;
+    try{
+        decoded = jwt.verify(token,'souvikarpita');
+    }catch(e){
+        return Promise.reject();
+        // return new Promise((resolve,reject)=>{
+        //     reject();
+        // });
+    }
+   return User.findOne({
+       '_id':decoded._id,
+       'tokens.token':token,
+       'tokens.access':'auth'
+   });
+ };
+
+var User = mongoose.model('User',UserSchema);
 
  module.exports={
     User
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const ObjectID = require('mongodb').ObjectID;
 var {mongoose} = require('./db/mongoose');
 var {User} = require('./models/user');
 var {Todo} = require('./models/todo');
+var {authenticate} = require('./middleware/authenticate');
 
 var app= express();
 var port = process.env.PORT || 3000;
@@ -92,10 +93,19 @@ app.post('/users',(req,res)=>{
     var body = _.pick(req.body,['email','password']);
     var user = new User(body);
     user.save().then((user)=>{
-        res.status(200).send({user});
+        return user.generateAuthToken(); 
+    }).then((token)=>{
+        res.header('x-auth',token).status(200).send({user});
     }).catch((e)=>{
          res.status(404).send(e);
     })
+});
+
+
+
+
+app.get('/users/me',authenticate,(req,res)=>{
+    res.send({user:req.user});
 });
 
 app.listen(port,()=>{
