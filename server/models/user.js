6ebc8a5796e1f2118c245b6d27d8bdf0d50ba886
souvikbanerjee86 <souvik.mlindia@gmail.com>
2018-07-08 09:16:19 +0530

    eigth Commit

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,7 @@ const validator = require('validator');
 const jwt = require('jsonwebtoken');
 
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
     email:{
@@ -74,6 +75,20 @@ var UserSchema = new mongoose.Schema({
        'tokens.access':'auth'
    });
  };
+
+ UserSchema.pre('save',function(next){
+    var user = this;
+    if(user.isModified('password')){
+        bcrypt.genSalt(100,(err,salt)=>{
+            bcrypt.hash(user.password,salt,(err,hash)=>{
+                user.password = hash;
+                next();
+            })
+        });
+    }else{
+        next();
+    }
+ });
 
 var User = mongoose.model('User',UserSchema);
 
